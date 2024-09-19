c1864b7fff6e5d49fb3af5f18a62c45bc7454753
Charlie Greenman <charlie@razroo.com>
2024-09-19 02:07:51 -0400

    Create starter files for angular-18.2.0

---
+++ apps/asjdajsdijdsad-asdakdkasdjsad-hello-asdkasdkads/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'asjdajsdijdsad-asdakdkasdjsad-hello-asdkasdkads-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'asjdajsdijdsad-asdakdkasdjsad-hello-asdkasdkads';
+}
